465c1605ceef5703c088419fba10de5b0d5986dc
Marcio <matiusbv@gmail.com>
2024-06-02 11:29:19 -0300

    Estilos globais

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./components/styles/global";
 import { defaultTheme } from "../src/components/styles/themes/default";
 
 export function App() {
@@ -11,6 +12,7 @@ export function App() {
       <Button variant="secondary"/>
       <Button variant="success" />
       <Button variant="danger"/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
